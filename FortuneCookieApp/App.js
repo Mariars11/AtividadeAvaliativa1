d7f52c70e944f6b5ccc042a3715d4e793cd23386
Maria Clara <maria.clara@bcb.gov.br>
2024-09-12 20:33:52 -0300

    Adicionando imagens do biscoito e tornando onPress funcionou para trocar imagens

--- FortuneCookieApp/App.js
+++ FortuneCookieApp/App.js
@@ -1,20 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Button } from 'react-native';
+import { useState  } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let urlBiscoitoAberto = './assets/BiscoitoAberto.jpeg';
+  let urlBiscoitoFechado = './assets/BiscoitoFechado.jpeg';
+  let [isPressed, setPressed] = useState(false);
+  if(!isPressed){
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.divImagemTexto}>
+            <Text style={styles.textLabel}>Biscoito da Sorte - Fechado</Text>
+            <Image style={styles.imagem} source={require(urlBiscoitoFechado)}/>
+            <View style={styles.botao}>
+              <Button  
+                title="Quebre o Biscoito"
+                onPress={() => setPressed(true)}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+  else{
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={styles.divImagemTexto}>
+            <Text style={styles.textLabel}>Biscoito da Sorte - Aberto</Text>
+            <Image style={styles.imagem} source={require(urlBiscoitoAberto)}/>
+            <View style={styles.botao}>
+            <Button  
+                title="Feche o biscoito"
+                onPress={() => setPressed(false)}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  imagem:{
+    maxWidth: 200,
+    maxHeight: 200,
+    marginLeft: 100,
+    textAlign: 'center',
+    backgroundColor: 'red'
+  },
+  textLabel:{
+      color: 'white',
+      fontSize: 30,
+      textAlign: 'center',
+  },
+  divImagemTexto:{
+      marginTop: 90
+  },
+  botao:{
+    marginTop: 20,
+    maxWidth: 100,
+    alignSelf: 'center',
+    color: '#00f'
+  }
 });
