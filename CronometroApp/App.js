fa1e98a9dfbd38e6f79d5a23b591a7e66f61ad9b
Maria Clara <maria.clara@bcb.gov.br>
2024-09-13 10:47:14 -0300

    Diferenciando milisegundos de segundos de minutos

--- CronometroApp/App.js
+++ CronometroApp/App.js
@@ -2,24 +2,36 @@ import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  let [count, setCount] = useState(0);
+  let [miliS, setMili] = useState(0);
+  let [segundos, setSegundos] = useState(0);
+  let [minutos, setMinutos] = useState(0);
 
   startTimer = () => {
      this.interval = setInterval(() =>{
-      setCount(count = count + 1);
-  }, 1000)};
+        setMili(miliS = miliS + 1);
+        if((miliS % 100) === 0){
+            setMili(miliS = 0);
+            setSegundos(segundos = segundos + 1);
+        }
+        if(segundos > 0 && (segundos % 60) === 0){
+          setSegundos(segundos = 0);
+          setMinutos(minutos = minutos + 1);
+        }
+    }, 10)};
 
   stopTimer = () => {
     clearInterval(this.interval);
   };
   reloadTimer = () => {
     clearInterval(this.interval);
-    setCount(count = 0);
+    setSegundos(segundos = 0);
+    setMili(miliS = 0);
+    setMinutos(minutos = 0);
     this.startTimer();
   };
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 50}}>{count} s</Text>
+      <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 50}}>{minutos} m {segundos} s {miliS} ms</Text>
       <View style={styles.divBtns}>
         <Button title='Iniciar' color='blue' onPress={()=> this.startTimer()}/>
         <Button title='Parar' color='red' onPress={()=> this.stopTimer()}/>
